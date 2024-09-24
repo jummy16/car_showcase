867f0296602d17eb1b41a7fd093f19583348f97c
jummy16 <ogungbeolajumoke@gmail.com>
2024-09-24 08:24:20 +0100

    new update on car catalogue

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-const SearchBar = () => {
-    const handleSearch = () =>{
+import { useState } from "react";
+import SearchManufacturer from "./SearchManufacturer";
 
-    }
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState("")
+    const handleSearch = () =>{}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className="searchbar__item">
-
+            <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer}/>
         </div>
     </form>
   )
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,11 @@
+import { SearchManufacturerProps } from '@/types'
+import { Combobox, Transition } from '@headlessui/react'
+import React from 'react'
+
+const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerProps) => {
+  return (
+    <div>SearchManufacturer</div>
+  )
+}
+
+export default SearchManufacturer
